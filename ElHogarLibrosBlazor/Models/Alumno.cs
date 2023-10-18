c5daae548e3684c1763e8aa69b859cbeccfd92c2
Edwin Alejandro Galicia de la Quadra <edwin.galicia@agape.edu.sv>
2023-10-18 11:01:23 -0600

    Creacion de repositorio

---
+++ ElHogarLibrosBlazor/Models/Alumno.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElHogarLibrosBlazor.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El grado es requerido")]
+        public int GradoId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El Apellido es requerido")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "La imagen es requerida")]
+        public string Imagen { get; set; }
+    }
+}
